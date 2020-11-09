e204d82682c964cfcc505a15260992e8cef58023
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-11-09 22:33:24 +0000

    changed header

--- templates/common/header.php
+++ templates/common/header.php
@@ -5,14 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../../css/style.css" rel="stylesheet">
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
     <header>
-      <h1><a href="list_news.php">Pet Shelter</a></h1>
-      <h2><a href="list_news.php">Get your little animal right here, right now!</a></h2>
+      <div id="name_slogan">
+        <h1><a href="list_news.php">Pet Shelter</a></h1>
+        <h2><a href="list_news.php">Get your little animal right here, right now!</a></h2>
+      </div>
+      <div id="search">
+        <form action="/index.php">
+          <input type="text" placeholder="Procurar" name="search">
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+      </div>
       <div id="signup">
         <a href="register.html">Register</a>
-        <a href="login.html">Login</a>
-        <a href="">Search</a>
+        <a href="login.html">Login</a>   
       </div>
     </header>
