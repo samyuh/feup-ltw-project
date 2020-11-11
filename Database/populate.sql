106640c17e554ddba4a8332eb23a1a8bb83eda44
up201806490 <50011557+Hugomguima@users.noreply.github.com>
2020-11-11 16:53:49 +0000

    Fixed Primary Keys

--- Database/populate.sql
+++ Database/populate.sql
@@ -4,6 +4,8 @@ INSERT INTO User VALUES(3,'Digo Samuel Fernandes','samuel gambit');
 INSERT INTO User VALUES(4,'Beatriz Mendes','ziniwhini');
 INSERT INTO User VALUES(5,'Paulo Ribeiro','sporting campeao');
 INSERT INTO User VALUES(6,'Jorge Jesus','oitchentcha e otcho');
+INSERT INTO User VALUES(7,'Marcelo Rebelo de Sousa','pastel de nata');
+INSERT INTO User VALUES(8,'Eduardo Brito','tenhaides calma');
 
 
 INSERT INTO Pet VALUES(1,'Carlitos','dog','male','medium','brown');
@@ -15,23 +17,28 @@ INSERT INTO Pet VALUES(6,'Bob','dog','male','small','yellow');
 INSERT INTO Pet VALUES(7,'Whiskers','cat','male','small','black and white');
 INSERT INTO Pet VALUES(8,'Minie','dog','female','small','light brown');
 INSERT INTO Pet VALUES(9,'Mickey','dog','male','small','dark bown');
+INSERT INTO Pet VALUES(10,'Blitz','dog','male','small','black');
 
 
-INSERT INTO UserLookingPet VALUES(1,1);
-INSERT INTO UserLookingPet VALUES(2,2);
-INSERT INTO UserLookingPet VALUES(3,3);
-INSERT INTO UserLookingPet VALUES(4,4);
-INSERT INTO UserLookingPet VALUES(5,5);
-INSERT INTO UserLookingPet VALUES(6,6);
-INSERT INTO UserLookingPet VALUES(1,7);
+INSERT INTO UserLookingPet VALUES(5,1);
+INSERT INTO UserLookingPet VALUES(5,2);
+INSERT INTO UserLookingPet VALUES(6,3);
+INSERT INTO UserLookingPet VALUES(6,4);
+INSERT INTO UserLookingPet VALUES(7,5);
+INSERT INTO UserLookingPet VALUES(8,6);
+INSERT INTO UserLookingPet VALUES(8,7);
 
 INSERT INTO UserFoundPet VALUES(1,1,'cool dog');
 INSERT INTO UserFoundPet VALUES(1,2,'likes to play football');
 INSERT INTO UserFoundPet VALUES(1,3,'likes warm places');
 INSERT INTO UserFoundPet VALUES(2,4,'likes to cuddle');
 INSERT INTO UserFoundPet VALUES(2,5,'big but friendly');
-INSERT INTO UserFoundPet VALUES(3,6,'very fluffy');
-INSERT INTO UserFoundPet VALUES(4,7,'very calm');
+INSERT INTO UserFoundPet VALUES(2,6,'very fluffy');
+INSERT INTO UserFoundPet VALUES(3,7,'very calm');
+INSERT INTO UserFoundPet VALUES(3,8,'big but friendly');
+INSERT INTO UserFoundPet VALUES(4,9,'very fluffy');
+INSERT INTO UserFoundPet VALUES(4,10,'fast and energetic');
+
 
 INSERT INTO FavoritePet VALUES(1,1);
 INSERT INTO FavoritePet VALUES(2,2);
