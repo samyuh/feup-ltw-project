33f6e887bf8028afcad16f04d05b68119e084387
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-11-15 00:28:15 +0000

    Logout and header with user info

--- templates/common/header.php
+++ templates/common/header.php
@@ -10,9 +10,23 @@
     <header>
       <h1><a href="index.php">Pet Shelter</a></h1>
       <h2><a href="index.php">Get your little animal right here, right now!</a></h2>
-      <div id="signup">
-        <a href="../../register.php">Register</a>
-        <a href="../../login.php">Login</a>
+      <?php 
+      if (!array_key_exists('username', $_SESSION) || empty($_SESSION['username'])) {
+        ?>
+        <div id="signup">
+          <a href="../../register.php">Register</a>
+          <a href="../../login.php">Login</a>
+        </div>
+        <?php } else { ?>
+        <div id="Profile">
+          <h1><?= $_SESSION['username']?></h1>
+        </div>
+        <div id="search">
+          <a href="../../action_logout.php">Logout</a>
+        </div>
+        <?php } ?>
+      
+      <div id="search">
         <a href="">Search</a>
       </div>
     </header>
