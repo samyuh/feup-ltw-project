8428a07879ef0b1338f9d15dae94865062066d84
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-11-27 09:19:03 +0000

    Update dogProfile

---
+++ dogProfile.php
@@ -0,0 +1,9 @@
+<?php
+  session_start();
+  include_once('database/connection.php');
+
+  include_once('templates/common/header.php');
+  //include_once('templates/home/homepage.php');
+  include_once('templates/profile/dog_profile.php');
+  include_once('templates/common/footer.php');
+?>
