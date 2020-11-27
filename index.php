ca895a7f78eef221232d9c50cd41b85d1e12848f
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-11-27 09:12:09 +0000

    Merge branch 'main-html-pages' into dogInformation

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  session_start();
+  include_once('database/connection.php');
+
+  include_once('templates/common/header.php');
+
+  //include_once('templates/home/homepage.php');
+  include_once('templates/profile/profile.php');
+  include_once('templates/common/footer.php');
+?>
--- templates/common/header.php
+++ templates/common/header.php
@@ -4,8 +4,12 @@
     <title>Pet Shelter</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../../css/styleDog.css" rel="stylesheet">
+    <link href="../../css/style.css" rel="stylesheet">
     <link href="../../css/dogInformation.css" rel="stylesheet">
+    <link href="../../css/layout.css" rel="stylesheet">
+    <link href="../../css/profile.css" rel="stylesheet">
+    <link href="../../css/footer.css" rel="stylesheet">
+    <link href="../../css/header.css" rel="stylesheet">
     <!-- Add icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
