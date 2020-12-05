d1d5c2c4f50731b0d1526b2cbac3a7896e21d812
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-12-05 14:16:09 +0000

    finished css for register page

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  include_once('templates/user/header_login.php');
+  include_once('templates/common/header_login_register.php');
   include_once('templates/user/login.php');
   include_once('templates/common/footer.php');
 ?>
--- templates/user/register.php
+++ templates/user/register.php
@@ -1,12 +1,26 @@
   <section id="register">
-    <h1>Register</h1>
-    <form action="action_register.php" method="post">
-      <label>
-        Username <input type="text" name="username">
-      </label>
-      <label>
-        Password <input type="password" name="password">
-      </label>
-      <input type="submit" value="Register">
-    </form>
+    <div class="hero-image">
+      <img src="../images/family-dog.jpeg" width="1920" height="950">
+    </div>
+    <div class="name_slogan">
+      <h1><a href="index.php">Pet Shelter</a></h1>
+      <h2><a href="index.php">Get your little animal right here, right now!</a></h2>
+    </div>
+    <div class="container_forms">
+      <form action="action_register.php" method="post">
+        <div class="username">
+          <label>
+            <input type="text" name="username" placeholder="Username">
+          </label>
+        </div>
+        <div class="password">
+          <label>
+            <input type="password" name="password" placeholder="Password">
+          </label>
+        </div>
+        <div class="submit">
+          <input type="submit" value="Register">
+        </div>
+      </form>
+    </div>
   </section>
