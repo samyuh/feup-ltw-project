915325d3979762a4d024143ad510f931c3f600ce
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-12-05 15:01:56 +0000

    first try in css for the homepage

--- templates/home/homepage.php
+++ templates/home/homepage.php
@@ -3,17 +3,17 @@
   <?php
     foreach($articles as $article) {?>
     <article>
-    <section id="profile">
-      <div class="image">
-        <img src="./images/dog.JPG" width="200" height="200" alt="">
-      </div>
-      <div class="info">
-        <p><a href=dog_profile.php?idPet=<?=$article['idPet']?>><?=$article['petName']?></a></p>
-        <p>Raça: <?=$article['specie']?></p>
-        <p>Idade: </p>
-        <p>Tamanho:  <?=$article['size']?></p>
-        <p>Cor:  <?=$article['color']?></p>
-        <p>Localização:  </p>
+    <section id="homepage_profile">
+      <img src="./images/dog.JPG" width="300" height="300" alt="" class="image">
+      <p><a href=dog_profile.php?idPet=<?=$article['idPet']?>><?=$article['petName']?></a></p>
+      <div class="overlay">
+        <div class="text">
+          <p>Raça: <?=$article['specie']?></p>
+          <p>Idade: </p>
+          <p>Tamanho:  <?=$article['size']?></p>
+          <p>Cor:  <?=$article['color']?></p>
+          <p>Localização:  </p>
+        </div>
       </div>
     </section>
     </article>
