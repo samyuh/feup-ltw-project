7a41ed3c16937c2a1bb526e8aed29cb68f8c0cf0
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-12-05 19:06:51 +0000

    finished footer

--- templates/common/footer.php
+++ templates/common/footer.php
@@ -1,5 +1,11 @@
 <footer>
-  <p>Pet Shelter, 2020</p>
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="index.php"> Pet Shelter</a>
+  </div>
+  <!-- Copyright -->
+
 </footer>
 </body>
 </html>
