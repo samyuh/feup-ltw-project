a24e195157d4796bea314c69731f076c5e43197e
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-12-09 21:02:15 +0000

    improved add_pet form

--- templates/common/header.php
+++ templates/common/header.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../../scripts/search.js" defer> </script>
     <script src="../../scripts/update.js" defer> </script>
+    <script src="../../scripts/addPet.js" defer> </script>
    
   </head>
   <body>
