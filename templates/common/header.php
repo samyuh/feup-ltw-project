2e2d21110ab5e07bbe880d5acdac838da9ce890c
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-12-09 18:58:55 +0000

    css for add new pet

--- templates/common/header.php
+++ templates/common/header.php
@@ -14,6 +14,7 @@
     <link href="../../css/updateUser.css" rel="stylesheet">
     <link href="../../css/updatePet.css" rel="stylesheet">
     <link href="../../css/search_advanced.css" rel="stylesheet">
+    <link href="../../css/newPet.css" rel="stylesheet">
     <!-- Add icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../../scripts/search.js" defer> </script>
