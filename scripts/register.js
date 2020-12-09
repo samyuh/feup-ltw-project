e5dcae195985171e4c08caf40fad6f12db4e502e
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-12-09 20:08:58 +0000

    Update register.js

--- scripts/register.js
+++ scripts/register.js
@@ -15,16 +15,16 @@ function validateRegistration(event) {
     let location = form.querySelector('input[name="location"]').value
     let password = form.querySelector('input[name="password"]').value
 
+    let path = form.querySelector('input[name="image"]').value
+    var file = path.replace(/^.*\\/, "");
+
+
     let regex = RegExp(/^[a-zA-Z0-9]+$/)
-    let regexAge = RegExp(/^\d$/)
     let regexLocation = RegExp(/^[a-zA-Z0-9""]+$/)
     let regexPassword = RegExp(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/)
 
     if(!regex.test(username)) {
         alert("Invalid username. Use letters and numbers only.")
-    }
-    else if(!regexAge.test(age)) {
-        alert("Invalid Age. Must be a number.")
     }
     else if(!regexLocation.test(location)) {
         alert("Invalid location. Use letters, numbers and blank space only.")
@@ -32,11 +32,17 @@ function validateRegistration(event) {
     else if(!regexPassword.test(password)) {
         alert("Invalid password. Must contain at least a letter and a number.")
     }
+    else if(!isFileImage(file)){
+        alert("Invalid file. Must be an image.")
+    }
     else {
         form.submit()
     } 
 
-    function updateTextInput(val) {
-        document.getElementById("textInput").value=val; 
-    }
+    
+}
+
+function isFileImage(file) {
+    let f = file && file.split('.')[1]
+    return (f === 'jpeg')  || (f ==='jpg')  || (f === 'png') 
 }
