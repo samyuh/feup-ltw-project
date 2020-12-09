fe4296c0d306effa4cde5d3e5037ce7918d3b69d
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-12-09 18:49:34 +0000

    css for update user info

--- templates/user/update.php
+++ templates/user/update.php
@@ -1,30 +1,32 @@
-<section id="updateUsername">
-    <form id="updateUsernameForm" class="changeUsername" action="../../action/action_update_username.php" method="post">
-      <h1>Update Username</h1>  
-      <div id="usernameinputs">
-        <label>
-        New Username <input type="text" name="new_username" placeholder="New Username">
-        </label>
-        <label>
-        Actual Password <input type="password" name="password" placeholder="Current Password">
-        </label>
-      </div>
-      <button id="updateUsernameButton" type="submit" value="Update Username">
-    </form>
-</section>
+<section id="update">
+  <section id="updateUsername">
+      <form id="updateUsernameForm" class="changeUsername" action="../../action/action_update_username.php" method="post">
+        <h1>Update Username</h1>  
+        <div id="usernameinputs">
+          <label>
+          New Username<input type="text" name="new_username">
+          </label>
+          <label>
+          Actual Password<input type="password" name="password">
+          </label>
+        </div>
+        <button id="updateUsernameButton" type="submit" value="Update Username">Update Username</button>
+      </form>
+  </section>
 
-<section id="updatePassword">
-   <form id="updatePasswordForm" class="changePassword" act ion="../../action/action_update_password.php" method="post"> 
-    <h1>Update Password</h1> 
-      <label>
-        <input type="password" name="new_password" placeholder="New Password">
-      </label>
-      <label>
-        <input type="password" name="confirm_password" placeholder="Confirm New Password">
-      </label>
-      <label>
-        <input type="password" name="password"  placeholder="Current Password">
-      </label>
-      <button id="updatePasswordButton" type="submit" value="Update Password">
-    </form>
+  <section id="updatePassword">
+    <form id="updatePasswordForm" class="changePassword" act ion="../../action/action_update_password.php" method="post"> 
+      <h1>Update Password</h1> 
+        <label>
+          New Password<input type="password" name="new_password">
+        </label>
+        <label>
+          Confirm New Password<input type="password" name="confirm_password">
+        </label>
+        <label>
+          Current Password<input type="password" name="password">
+        </label>
+        <button id="updatePasswordButton" type="submit" value="Update Password">Update Password</button>
+      </form>
+  </section>
 </section>
