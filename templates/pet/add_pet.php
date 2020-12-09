7af0d80f194a14c254d3d9f8adc778afe2a841f3
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-12-09 18:58:44 +0000

    css for add new pet

--- templates/pet/add_pet.php
+++ templates/pet/add_pet.php
@@ -1,11 +1,13 @@
 <section id="add">
-    <h1>Insert Pet Info</h1>
-    <form action="../../action/action_add_pet.php" method="post">
-        <label>Update Name <input type="text" name="npetName"></label>
-        <label>Update Specie <input type="text" name="nspecie"></label>
-        <label>Update Gender <input type="text" name="ngender"></label>
-        <label>Update Size <input type="text" name="nsize" ></label>
-        <label>Update color <input type="text" name="ncolor"></label>
-        <input type="submit" value="update">
-    </form>
+    <section id="petinfocontainer">
+        <h1>Insert Pet Info</h1>
+        <form class="inputsNewPet" action="../../action/action_add_pet.php" method="post">
+            Update Name <input type="text" name="npetName">
+            Update Specie <input type="text" name="nspecie">
+            Update Gender <input type="text" name="ngender">
+            Update Size <input type="text" name="nsize" >
+            Update color <input type="text" name="ncolor">
+            <input type="submit" value="Update">
+        </form>
+    </section>
 </section>
