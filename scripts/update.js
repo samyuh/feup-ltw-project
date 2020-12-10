33d5d0e2a1af6785ac065ff9ca8f96e9f5fe1d2b
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-12-10 00:13:12 +0000

    fixed missing variable declarations

--- scripts/update.js
+++ scripts/update.js
@@ -10,11 +10,11 @@ function validateUpdateUsername(e){
     e.preventDefault()
     let form = document.getElementById("updateUsernameForm")
 
-    username = form.querySelector('input[name="new_username"]').value
-    password = form.querySelector('input[name="password"]').value
+    let username = form.querySelector('input[name="new_username"]').value
+    let password = form.querySelector('input[name="password"]').value
 
-    regex = RegExp(/^[a-zA-Z0-9]+$/);  // All letters and numbers without blanck space
-    regexPassword = RegExp(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/)
+    let regex = RegExp(/^[a-zA-Z0-9]+$/);  // All letters and numbers without blanck space
+    let regexPassword = RegExp(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/)
 
     if(!regex.test(username)) {
         alert("Invalid username. Use letters and numbers only.")
@@ -33,12 +33,12 @@ function validateUpdatePassword(e){
     e.preventDefault()
     let form = document.getElementById("updatePasswordForm")
 
-    new_password = form.querySelector('input[name="new_password"]').value
-    confirm_password = form.querySelector('input[name="confirm_password"]').value
-    password = form.querySelector('input[name="password"]').value
+    let new_password = form.querySelector('input[name="new_password"]').value
+    let confirm_password = form.querySelector('input[name="confirm_password"]').value
+    let password = form.querySelector('input[name="password"]').value
 
     //regex = RegExp(/^[a-zA-Z0-9]+$/);  // All letters and numbers without blanck space
-    regexPassword = RegExp(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/)
+    let regexPassword = RegExp(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/)
 
     if(!regexPassword.test(new_password)){
         alert("Invalid new password. Must contain at least a letter and a number.")
