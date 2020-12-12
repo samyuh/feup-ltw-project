027a5c70a676ef24d1804e79c77a2cceaab8ae42
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-12-12 00:19:21 +0000

    finished organizing css for today
    tomorrow: start at profile.css

--- templates/common/header.php
+++ templates/common/header.php
@@ -16,6 +16,7 @@
     <link href="../../css/search_advanced.css" rel="stylesheet">
     <link href="../../css/newPet.css" rel="stylesheet">
     <link href="../../css/error404.css" rel="stylesheet">
+    <link href="../../css/containers.css" rel="stylesheet">
     <!-- Add icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../../scripts/search.js" defer> </script>
