5210a1f71cd6281f01812fdfd411d1682a58d758
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-12-14 21:15:41 +0000

    Validate advanced search and error 404

--- templates/home/error404page.php
+++ templates/home/error404page.php
@@ -1,6 +1,6 @@
-<section id="error404">
-    <img src="../images/sad-dog.jpg" alt="Sad Dog" width="400" height="400">
+<div id="error404">
     <h1>Error 404</h1>
     <h1>Page Not Found</h1>
-    <h2>Sorry, I ate this page</h2>
-</section>
+    <img src="../images/sad-dog.png" alt="Sad Dog" width="400" height="400">
+    <h3>Sorry, I ate this page</h3>
+</div>
--- templates/home/search_advanced.php
+++ templates/home/search_advanced.php
@@ -1,41 +1,34 @@
-<section id="advanced_search_page">
-  <aside id="side">
-    <fieldset id="advanced_search">
-          <h2>Advanced search</h2>
-          <form id="name_search" action="/search.php" method="post">
-            Insert Name: 
-              <input type="text" name="nameSearch" id="searchName">
+<div id="advanced_search_page">
+    <section id="advanced_search">
+      <h2>Advanced search</h2>
+        <form id="name_search" action="/search.php" method="post">
+          <label>Insert Name: <input type="text" name="nameSearch" id="searchName"></label>
+          <section id="species_options">
             Insert Species:
-            <section id="species_options">
-              <label>Dog <input class="hide" type="radio" name="speciesSearch" id="searchSpecies" value="dog"><i class="fa fa-fw fa-paw"></i>&nbsp;</label>
-              <label>Cat <input class="hide" type="radio" name="speciesSearch" id="searchSpecies"value="cat"><i class="fa fa-fw fa-paw"></i>&nbsp;</label>
-              <label>Any <input class="hide" type="radio" name="speciesSearch" id="searchSpecies"value="" checked="checked"><i class="fa fa-fw fa-paw"></i>&nbsp;</label>
-            </section>
+            <label>Dog <input class="hide" type="radio" name="speciesSearch" value="dog"><i class="fa fa-fw fa-paw"></i>&nbsp;</label>
+            <label>Cat <input class="hide" type="radio" name="speciesSearch" value="cat"><i class="fa fa-fw fa-paw"></i>&nbsp;</label>
+            <label>Any <input class="hide" type="radio" name="speciesSearch" value="" checked="checked"><i class="fa fa-fw fa-paw"></i>&nbsp;</label>
+          </section>
+          <section id="gender_options">
             Insert Gender:
-            <section id="species_options">
-              <label>Male <input class="hide" type="radio" name="genderSearch" id="searchGender" value="male"><i class="fa fa-fw fa-mars"></i>&nbsp;</label>
-              <label>Female <input class="hide" type="radio" name="genderSearch" id="searchGender" value="female"><i class="fa fa-fw fa-venus"></i>&nbsp;</label>
-              <label>Any <input class="hide" type="radio" name="genderSearch" id="searchGender"value=""  checked="checked"><i class="fa fa-fw fa-genderless"></i>&nbsp;</label>
-              </section>
-            Insert Size:
-            <section id="size_options">
-              <label>Small <input class="hide" type="radio" name="sizeSearch" id="searchSize" value="small"><i class="fa fa-fw fa-check-circle"></i>&nbsp;</label>
-              <label>Medium <input class="hide" type="radio" name="sizeSearch" id="searchSize" value="medium"><i class="fa fa-fw fa-check-circle"></i>&nbsp;</label>
-              <label>Large <input class="hide" type="radio" name="sizeSearch" id="searchSize" value="large"><i class="fa fa-fw fa-check-circle"></i>&nbsp;</label>
-              <label>Any <input class="hide" type="radio" name="sizeSearch" id="searchSize" value=""  checked="checked"><i class="fa fa-fw fa-check-circle"></i>&nbsp;</label>
+            <label>Male <input class="hide" type="radio" name="genderSearch" value="male"><i class="fa fa-fw fa-mars"></i>&nbsp;</label>
+            <label>Female <input class="hide" type="radio" name="genderSearch" value="female"><i class="fa fa-fw fa-venus"></i>&nbsp;</label>
+            <label>Any <input class="hide" type="radio" name="genderSearch" value=""  checked="checked"><i class="fa fa-fw fa-genderless"></i>&nbsp;</label>
             </section>
-            Insert Color 
-              <input type="text" name="colorSearch" id="searchSize">
-          </form>
-    </fieldset>
-    </aside>
-
-  <section id="displayPets">
-    <article>
-      <div class="profileGrid">
-      </div>
-    </article>
+          
+          <section id="size_options">
+            Insert Size:
+            <label>Small <input class="hide" type="radio" name="sizeSearch" value="small"><i class="fa fa-fw fa-check-circle"></i>&nbsp;</label>
+            <label>Medium <input class="hide" type="radio" name="sizeSearch" value="medium"><i class="fa fa-fw fa-check-circle"></i>&nbsp;</label>
+            <label>Large <input class="hide" type="radio" name="sizeSearch" value="large"><i class="fa fa-fw fa-check-circle"></i>&nbsp;</label>
+            <label>Any <input class="hide" type="radio" name="sizeSearch" value=""  checked="checked"><i class="fa fa-fw fa-check-circle"></i>&nbsp;</label>
+          </section>
+          <label>Insert Color <input type="text" name="colorSearch" id="searchSize"></label>
+        </form>
   </section>
-</section>
+
+  <div id="displayPets">
+  </div>
+</div>
 
 
