e607eee6e72b402940ee0b600cb05fb30b775686
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-12-15 13:56:23 +0000

    Update names on update

--- scripts/update.js
+++ scripts/update.js
@@ -1,14 +1,14 @@
 'use strict';
 
-if(document.getElementById('updateUsername') && document.getElementById('updatePassword')) {
-    document.getElementById('updateUsernameButton').addEventListener("click",validateUpdateUsername)
-    document.getElementById('updatePasswordButton').addEventListener("click",validateUpdatePassword)
+if(document.getElementById('update-username-form') && document.getElementById('update-password')) {
+    document.getElementById('update-username-button').addEventListener("click",validateUpdateUsername)
+    document.getElementById('update-password-button').addEventListener("click",validateUpdatePassword)
 }
 
 //This function validates the username, if it doesnt work a message is raised
 function validateUpdateUsername(e){
     e.preventDefault()
-    let form = document.getElementById("updateUsernameForm")
+    let form = document.getElementById("update-username-form")
 
     let username = form.querySelector('input[name="new_username"]').value
     let password = form.querySelector('input[name="password"]').value
@@ -22,10 +22,10 @@ function validateUpdateUsername(e){
     let passwordError = false
 
     if(!regex.test(username)) {
-        usernameError = updateError('updateNewUsernameError',"Invalid username. Use letters and numbers only.")
+        usernameError = updateError('update-new-username-error',"Invalid username. Use letters and numbers only.")
     }
     if(!regexPassword.test(password)) {
-        passwordError = updateError('updateActualPasswordError',"Invalid password. Must contain at least a letter and a number.")
+        passwordError = updateError('update-actual-password-error',"Invalid password. Must contain at least a letter and a number.")
     }
     if(!usernameError && !passwordError){
         form.submit()
@@ -45,20 +45,17 @@ function updateError(id,message){
 
 
 function clearUpdateHTML(){
-    document.getElementById('updateNewUsernameError').innerHTML = ''
-    document.getElementById('updateActualPasswordError').innerHTML = ''
-    document.getElementById('updateNewPasswordError').innerHTML = ''
-    document.getElementById('updateConfirmPasswordError').innerHTML = ''
-    document.getElementById('updateCurrentPasswordError').innerHTML = ''
+    document.getElementById('update-new-username-error').innerHTML = ''
+    document.getElementById('update-actual-password-error').innerHTML = ''
+    document.getElementById('update-new-password-error').innerHTML = ''
+    document.getElementById('update-confirm-password-error').innerHTML = ''
+    document.getElementById('update-current-password-error').innerHTML = ''
 }
-
-
-
 
 //This function validates the username, if it doesnt work a message is raised
 function validateUpdatePassword(e){
     e.preventDefault()
-    let form = document.getElementById("updatePasswordForm")
+    let form = document.getElementById("update-password-form")
 
     let new_password = form.querySelector('input[name="new_password"]').value
     let confirm_password = form.querySelector('input[name="confirm_password"]').value
@@ -74,11 +71,11 @@ function validateUpdatePassword(e){
     let passError = false
 
     if(!regexPassword.test(new_password)){
-        newPassError = updateError("updateNewPasswordError","Invalid new password. Must contain at least a letter and a number.")
+        newPassError = updateError("update-new-password-error","Invalid new password. Must contain at least a letter and a number.")
     }if(!regexPassword.test(confirm_password)){
-        confPassError = updateError("updateConfirmPasswordError","Invalid password confirmation. Must contain at least a letter and a number.")
+        confPassError = updateError("update-confirm-password-error","Invalid password confirmation. Must contain at least a letter and a number.")
     }if(!regexPassword.test(password)){
-        passError = updateError("updateCurrentPasswordError","Invalid password. Must contain at least a letter and a number.")
+        passError = updateError("update-current-password-error","Invalid password. Must contain at least a letter and a number.")
     }if(!newPassError && !confPassError && !passError){
         form.submit()
     } 
--- templates/user/update.php
+++ templates/user/update.php
@@ -1,77 +1,78 @@
 <div id="update-page">
   <article class="update-form">
         <h2>Update Username</h2> 
-        <form id="updateUsernameForm" class="change-username" action="../../action/action_update_username.php?token=<?=$_SESSION['csrf']?>" method="post">
-          <section id="updateNewUsernameError">
+        <form id="update-username-form" class="change-username" action="../../action/action_update_username.php?token=<?=$_SESSION['csrf']?>" method="post">
+          <section id="update-new-username-error">
           </section>
           <input type="text" name="new_username" placeholder="New Username">
             
-          <section id="updateActualPasswordError">
+          <section id="update-actual-password-error">
           </section>
           <input type="password" name="password" placeholder="Actual Password">
             
-          <button id="updateUsernameButton" type="submit" value="Update Username">Update Username</button>
+          <button id="update-username-button" type="submit" value="Update Username">Update Username</button>
         </form>
     </article>
 
     <article class="update-form">
         <h2>Update Password</h2>
-        <form id="updatePasswordForm" class="change-password" action="../../action/action_update_password.php?token=<?=$_SESSION['csrf']?>" method="post">
+        <form id="update-password-form" class="change-password" action="../../action/action_update_password.php?token=<?=$_SESSION['csrf']?>" method="post">
         
-          <section id="updateNewPasswordError">
+          <section id="update-new-password-error">
           </section>
           <input type="password" name="new_password" placeholder="New Password">
 
-          <section id="updateConfirmPasswordError">
+          <section id="update-confirm-password-error">
           </section>
           <input type="password" name="confirm_password" placeholder="Confirm New Password">
 
-          <section id="updateCurrentPasswordError">
+          <section id="update-current-password-error">
           </section>
           <input type="password" name="password" placeholder="Current Password">
-          <button id="updatePasswordButton" type="submit" value="Update Password">Update Password</button>
+          <button id="update-password-button" type="submit" value="Update Password">Update Password</button>
         </form>
     </article>
 
     <article class="update-form">
         <h2>Update Information</h2>
-        <form id="updateInformationForm" class="change-information" action="../../action/action_update_information.php?token=<?=$_SESSION['csrf']?>" method="post" enctype="multipart/form-data">
+        <form id="update-information-form" class="change-information" action="../../action/action_update_information.php?token=<?=$_SESSION['csrf']?>" method="post" enctype="multipart/form-data">
 
-          <section id="updateGender">
+          <section id="update-gender">
           </section>
           Male <input type="radio" name="gender" value="male">
           Female <input type="radio" name="gender" value="female">
 
-          <section id="updateAge">
+          <section id="update-age">
           </section>
           Age <input type="range" name="age" id="registerAge" value="20" min="0" max="99" oninput="this.nextElementSibling.value = this.value">
           <output>20</output>
 
 
-          <section id="updateLocation">
+          <section id="update-location">
           </section>
           <input type="text" name="location" placeholder="Location">
 
-          <section id="updateImage">
+          <section id="update-image">
           </section>
           <input type="file" name="image" accept="image/*" onchange="loadFile(event)">>
           <img id="output" src="#" style="max-height:15em; max-width:15em;" alt="Image Preview" />
-          <section id="updateActualPasswordInformationError">
+          
+          <section id="update-actual-password-information-error">
           </section>
           <input type="password" name="password" placeholder="Password">
 
-          <button id="updateInformationButton" type="submit" value="Update Password">Update Information</button>
+          <button id="update-information-button" type="submit" value="Update Password">Update Information</button>
         </form>
     </article>
 
     <article class="update-form">
         <h2>Delete Profile</h2>
-        <form id="deleteProfileForm" class="delete-profile" action="../../action/action_delete_profile.php?token=<?=$_SESSION['csrf']?>" method="post">
+        <form id="delete-profile-form" class="delete-profile" action="../../action/action_delete_profile.php?token=<?=$_SESSION['csrf']?>" method="post">
           
 
           <input type="password" name="password" placeholder="Actual Password">
 
-          <button id="deleteProfileButton" type="submit" value="Update Password">Delete Profile Permanently</button>
+          <button id="delete-profile-button" type="submit" value="Update Password">Delete Profile Permanently</button>
         </form>
     </article>
 </div>
