7cb58f97c461bd0f42bb56f36d7cd2c0afa6203f
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-12-16 22:27:10 +0000

    Fixed Notification bar

--- templates/common/header.php
+++ templates/common/header.php
@@ -86,10 +86,10 @@
 <button class="btn-toggle">Toggle Dark-Mode</button>
 <script>
 // Select the button
-const btn = document.querySelector(".btn-toggle");
+const btnDark = document.querySelector(".btn-toggle");
 
 // Listen for a click on the button
-btn.addEventListener("click", function() {
+btnDark.addEventListener("click", function() {
   // Then toggle (add/remove) the .dark-theme class to the body
   document.body.classList.toggle("dark-theme");
 });
