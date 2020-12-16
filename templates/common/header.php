950d30d6062ce1b659b8be617ab3ee4bc250bb67
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-12-16 19:29:44 +0000

    Start implemmenting dark mode

--- templates/common/header.php
+++ templates/common/header.php
@@ -82,3 +82,15 @@
         </div> 
       </div>
     </header>
+
+<button class="btn-toggle">Toggle Dark-Mode</button>
+<script>
+// Select the button
+const btn = document.querySelector(".btn-toggle");
+
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle("dark-theme");
+});
+</script>
