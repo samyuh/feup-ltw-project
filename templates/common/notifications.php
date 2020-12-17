0ca57df59e7a95fedce099c8c18bc23f5a98d67f
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-12-17 17:26:03 +0000

    changed notifications css

--- templates/common/notifications.php
+++ templates/common/notifications.php
@@ -6,12 +6,13 @@
             <?php $notifications = getUserNotifications($_SESSION['user']['idUser']); ?>
             <?php foreach($notifications as $not) { ?>
               <article>
+                <i class="fa fa-fw fa-paw"></i>
                 <img src="../images/user/user-<?=$not['idUser']?>.jpg" width="20" height="20" alt="User">
                 <span><?= htmlentities($not['username'])  ?> wants to adopt </span>
                 <img src="../images/pet-profile/pet-<?=$not['idPet']?>/profile.jpg" width="20" height="20" alt="Pet">
                 <span><?= htmlentities($not['petName']) ?>!</span>
                 <a href="pet_profile.php?idPet=<?=$not['idPet']?>"> Go to the pet page! </a>
-              <article>
+              </article>
             <?php } ?> 
           </div>
         </div>
