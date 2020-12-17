2895487e8d31593702c42bfd991d45bde6b4aad2
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-12-17 13:21:45 +0000

    fix borders and photos slide on pet page

--- templates/pet/pet_information.php
+++ templates/pet/pet_information.php
@@ -42,8 +42,10 @@
                 <img src="../../images/pet-profile/pet-<?=$pet['idPet']?>/photo-<?=$photo['idPhoto']?>.jpg" alt="Failed displaying pet image" width="400" height="400">
               </div>
             <?php } ?>
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            <?php if(!empty($photos)) { ?>
+              <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+              <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            <?php } ?>
           </div>
         </section>
       </section>
